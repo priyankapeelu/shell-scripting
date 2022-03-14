e29726502a3baa6d755e8271d5bb59569651aed9
priyankapeelu <priyankapeelu@icloud.com>
2022-03-14 19:54:58 +0530

    Adding Catalogue Setup

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -40,7 +40,8 @@ StatCheck $?
 Print "Setup SystemD File"
 sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'
 /home/roboshop/catalogue/systemd.service &>>${LOG_FILE} && mv
-/home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue
+/home/roboshop/catalogue/systemd.service
+/etc/systemd/system/catalogue
 .service &>>${LOG_FILE}
 StatCheck $?
 
