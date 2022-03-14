ec85beb23b2196017154f8e502906395034c320b
priyankapeelu <priyankapeelu@icloud.com>
2022-03-14 16:31:07 +0530

    Adding Catalogue Setup

---
+++ roboshop/components/catalogue.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+source components/common.sh
+
+#1. So let's switch to the `roboshop` user and run the following commands.
+#
+#```bash
+#$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+#$ cd /home/roboshop
+#$ unzip /tmp/catalogue.zip
+#$ mv catalogue-main catalogue
+#$ cd /home/roboshop/catalogue
+#$ npm install
+#
+#```
+#
+#1. Update SystemD file with correct IP addresses
+#
+#   Update `MONGO_DNSNAME` with MongoDB Server IP
+#
+#2. Now, lets set up the service with systemctl.
+
+Print "Configure Yum repos"
+# curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>${LOG_FILE}
+StatCheck $?
+
+Print "Install NodeJS"
+# yum install nodejs gcc-c++ -y &>>${LOG_FILE}
+StatCheck $?
+
+Print "Add Application User"
+useradd ${APP_USER}
+StatCheck $?
+
+Print "Download App Component"
+curl -s -L -o /tmp/catalogue.zip "https://github
+ .com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
+StatCheck $?
+
+Print "Cleanup Old Contnent"
+rm -rf /home/roboshop/catalogue &&>>${LOG_FILE}
+StatCheck $?
+
+Print "Extract App Content"
+cd /home/roboshop && unzip -o /tmp/catalogue.zip &>>&>>${LOG_FILE} && mv catalogue-main catalogue &>>${LOG_FILE}
+StatCheck $?
+
+Print "Install App Dependencies"
+cd /home/roboshop/catalogue &&>>${LOG_FILE} && npm install &>>${LOG_FILE}
+StatCheck $?
+
+
