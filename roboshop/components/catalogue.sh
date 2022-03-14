a3c5e2c4d2f7e6270fe849d71e9826ebd122619c
priyankapeelu <priyankapeelu@icloud.com>
2022-03-14 19:32:48 +0530

    Adding Catalogue Setup

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -38,5 +38,9 @@ chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
 StatCheck $?
 
 Print "Setup SystemD File"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'
+/home/roboshop/catalogue/systemd.service && mv home /roboshop/catalogue/systemd.service/etc/systemd/system/catalogue.service
+StatCheck $?
 
-
+Print "Restart Catalogue Service"
+systemctl daemon-reload &>>${LOG_FILE} && systemctl restart catalogue &>>${LOG_FILE} && systemctl enable catalogue &>>${LOG_FILE}
