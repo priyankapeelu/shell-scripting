6960a7ad7be3148a973518be8f832dabb19fb1cb
priyankapeelu <priyankapeelu@icloud.com>
2022-03-15 19:19:57 +0530

    setting up rabbitmq script

---
+++ roboshop/components/rabbitma.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source components/common.sh
+
+Print "configure yum repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>{LOG_FILE}
+StatCheck $?
+
+Print "start Erlang rabbitmq"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>{LOG_FILE}
+StatCheck $?
+
+Print "start rabbitmq service"
+systemctl enable rabbitmq-server &>>{LOG_FILE} && systemctl start rabbitmq-server &>>{LOG_FILE}
+StatCheck $?
+
+Print "create application user"
+rabbitmqctl add_user roboshop roboshop123 &>>{LOG_FILE}
+StatCheck $?
