d87f0401ef8552816509e88ce744bbbc291d3f81
priyankapeelu <priyankapeelu@icloud.com>
2022-03-15 19:37:17 +0530

    setting up rabbitmq script

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -3,21 +3,21 @@
 source components/common.sh
 
 Print "start Erlang rabbitmq"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>{LOG_FILE}
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>${LOG_FILE}
 StatCheck $?
 
 Print "configure yum repos"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>{LOG_FILE}
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${LOG_FILE}
 StatCheck $?
 
 Print "Install RabbitMQ-Server"
-yum install rabbitmq-server -y
+yum install rabbitmq-server -y &>>${LOG_FILE}
 StatCheck $?
 
 Print "start rabbitmq service"
-systemctl enable rabbitmq-server &>>{LOG_FILE} && systemctl restart rabbitmq-server &>>{LOG_FILE}
+systemctl enable rabbitmq-server &>>${LOG_FILE} && systemctl restart rabbitmq-server &>>${LOG_FILE}
 StatCheck $?
 
 Print "create application user"
-rabbitmqctl add_user roboshop roboshop123 &>>{LOG_FILE}
+rabbitmqctl add_user roboshop roboshop123 &>>${LOG_FILE}
 StatCheck $?
