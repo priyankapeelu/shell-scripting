e16aa15ef938b91ec0ddeed2cc88bef473380750
priyankapeelu <priyankapeelu@icloud.com>
2022-03-21 19:33:40 +0530

    create EC2 Instance Script

--- create_ec2.sh
+++ create_ec2.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-echo $AMI_ID
+if [ -z "$1" ]; then
+  echo -e "\e[31m input machine name is needed\e[0m"
+  exit 1
+fi
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro
+COMPONENT=$1
+
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+
+echo $AMI_ID
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{key=Name,Value=${COMPONENT}}]" | jq
