f153e9ec47357f167e8773891972467f21d70dbe
priyankapeelu <priyankapeelu@icloud.com>
2022-03-21 20:25:08 +0530

    create EC2 Instance Script

--- create_ec2.sh
+++ create_ec2.sh
@@ -6,9 +6,19 @@ if [ -z "$1" ]; then
 fi
 
 COMPONENT=$1
+ZONE_ID="Z05705092FH54GCN798S"
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=launch-wizard-1 | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 echo $AMI_ID
-#aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{key=Name, Value=${COMPONENT}}]" | jq
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=${COMPONENT}}]" | jq
+PRIVATE_IP=$(aws ec2 run-instances \
+    --image-id ${AMI_ID} \
+    --instance-type t2.micro \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=${COMPONENT}}]"
+    --instance-market-options "MarketType:spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehaviour=stop" \
+    --security-group-ids ${SGID} \
+    | jq | '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+sed -e "s-/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id $(ZONE_ID) --change-batch file://tmp.record.json
